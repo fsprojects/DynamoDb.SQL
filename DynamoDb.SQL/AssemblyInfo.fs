905099cfe392c35fd9bba9a14af6c04a5a549ad5
theburningmonk <theburningmonk@gmail.com>
2013-05-06 02:53:28 +0100

    - updated assembly version

--- DynamoDb.SQL/AssemblyInfo.fs
+++ DynamoDb.SQL/AssemblyInfo.fs
@@ -11,8 +11,8 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyProduct("DynamoDb.SQL")>]
 [<assembly: AssemblyCopyright("Copyright @ Yan Cui 2012")>]
-[<assembly: AssemblyVersion("1.1.3")>]
-[<assembly: AssemblyFileVersion("1.1.3")>]
+[<assembly: AssemblyVersion("1.2.1")>]
+[<assembly: AssemblyFileVersion("1.2.1")>]
 [<assembly: InternalsVisibleTo("DynamoDb.SQL.Tests")>]
 
 do ()
